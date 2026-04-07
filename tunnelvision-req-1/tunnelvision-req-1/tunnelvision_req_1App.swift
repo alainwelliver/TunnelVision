9a1b0876321ad05d102330b6d70756e924289617
tomasou123 <tomas.ou123@gmail.com>
2026-04-06 23:01:23 -0400

    deleted req 5 temp files + added requiremnt 1 own folder and build

---
+++ tunnelvision-req-1/tunnelvision-req-1/tunnelvision_req_1App.swift
@@ -0,0 +1,17 @@
+//
+//  tunnelvision_req_1App.swift
+//  tunnelvision-req-1
+//
+//  Created by Tomas Ou on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct tunnelvision_req_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            HelloWorldView()
+        }
+    }
+}
